02c254344124b46d933afe872cdbd02376940ad7
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-04 10:26:53 -0400

    Change duration of scrollTop animation
    'fast' felt too abrupt; changed to 400ms

--- assets/scripts/home/ui.js
+++ assets/scripts/home/ui.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// jQuery plugin from
+// jQuery plugin adapted from
 // http://stackoverflow.com/questions/4801655/how-to-go-to-a-specific-element-on-page
 (function($) {
     $.fn.goTo = function() {
         $('html, body').animate({
             scrollTop: $(this).offset().top + 'px'
-        }, 'fast');
+        }, 400);
         return this; // for chaining...
     };
 })(jQuery);
